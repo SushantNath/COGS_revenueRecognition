27edf9ab904293d989bf273dc7ead77acc4fd747
Sushant Nath <sushbhubaneswar1@gmail.com>
2020-09-04 16:15:07 +0000

    validation given for invoice type

--- webapp/controller/information.controller.js
+++ webapp/controller/information.controller.js
@@ -7,12 +7,41 @@ sap.ui.define([
 		onInit: function () {
 
 		},
-		
+/* code to check validation and navigate to Outbound details screen */
 		onClickGetInformation: function () {
-				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-					oRouter.navTo("outboundDetails");
-					console.log("Inside navigation loop");
+			
+			 var requiredInputs = this.returnIdListOfRequiredFields();
+            var passedValidation = this.validateEventFeedbackForm(requiredInputs);
+            if(passedValidation === false)
+            {
+                //show an error message, rest of code will not execute.
+                return false;
+            }
 
-		}
+		},
+		 returnIdListOfRequiredFields: function()
+        {
+           var requiredInputs = [];
+            $('[data-required="true"]').each(function(){
+                requiredInputs.push($(this).context.id);
+            });
+            return requiredInputs;
+        },
+        validateEventFeedbackForm: function(requiredInputs) {
+        	var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                var _self = this;
+                var valid = true;
+                requiredInputs.forEach(function (input) {
+                    var sInput = _self.getView().byId(input);
+                    if (sInput.getValue() == "" || sInput.getValue() == undefined) {
+                        valid = false;
+                        sInput.setValueState("Error");
+                    }
+                    else {
+                        oRouter.navTo("outboundDetails");
+                    }
+                });
+                return valid;
+        }
 	});
 });
