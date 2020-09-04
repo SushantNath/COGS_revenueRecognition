8ebec49aef5dabd5d661673b95bd236ee7d57eb0
Sushant Nath <sushbhubaneswar1@gmail.com>
2020-09-04 10:12:35 +0000

    navigationEnabled

--- webapp/controller/information.controller.js
+++ webapp/controller/information.controller.js
@@ -6,6 +6,13 @@ sap.ui.define([
 	return Controller.extend("com.sap.revenueRecognition.cogs_revenueRecognition.controller.information", {
 		onInit: function () {
 
+		},
+		
+		onClickGetInformation: function () {
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+					oRouter.navTo("outboundDetails");
+					console.log("Inside navigation loop");
+
 		}
 	});
 });
